bab7b44c888bacfed5aea81fd90d405ba394c392
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-03-07 09:25:42 +0530

    hi

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>name : vansh thapar</h1>
+    <h2>contact details:</h2>
+    <ul>
+      <li>phone number : 9876543210</li>
+      <li>email: vanshthapar@gmail.com</li>
+      <li>address: white house U.S.A</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
